808f4bf86dfcbbff5466e1b9c8ab89579214d641
dtrej9999 <dtrej9999@tamu.edu>
2020-03-03 10:08:17 -0600

    added random moving robot as ghost

---
+++ RealChase/Assets/randomMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class randomMove : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public float timer;
+    public int newtarget;
+    public float speed;
+    public Vector3 target;
+
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= newtarget){
+            newTarget();
+            timer = 0;
+        }
+    }
+
+    void newTarget(){
+        float myX = gameObject.transform.position.x;
+        float myZ = gameObject.transform.position.z;
+
+        float xPos = myX + Random.Range(myX - 100, myX + 100);
+        float zPos = myZ + Random.Range(myZ - 100, myZ + 100);
+
+        target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+
+        nav.SetDestination(target);
+    }
+}
