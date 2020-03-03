7c841cd666fdd159ea4f70d6b904231f908628a0
dtrej9999 <dtrej9999@tamu.edu>
2020-03-02 21:09:37 -0600

    mini map functionality

---
+++ RealChase/Assets/miniMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniMap : MonoBehaviour
+{
+    public Camera MapCamera;
+
+    void Start()
+    {
+        MapCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M)){
+            MapCamera.enabled = !MapCamera.enabled;
+        }
+    }
+}
