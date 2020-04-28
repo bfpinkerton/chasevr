58a4d0266a11422b7c306c3fcc0a73f8d51571e5
Samantha <samantharhay@gmail.com>
2020-04-27 21:04:08 -0500

    maybe fix

--- RealChase/Assets/Scenes/menu/Instructions_button.cs
+++ RealChase/Assets/Scenes/menu/Instructions_button.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 using UnityEngine.SceneManagement;
+
 public class Instructions_button : MonoBehaviour
 {
 	public bool end;
 	public int frames;
-    
+    public static Vector3 StartPosition;
+	Player _instance;
 	void Start(){
 		end = false;
 		frames = 0;
+		_instance = FindObjectOfType<Player>();
+		StartPosition = _instance.trackingOriginTransform.position;
 	}
     void Update()
     {
@@ -28,6 +33,7 @@ public class Instructions_button : MonoBehaviour
 		if((collision.transform.name == "Player")||(collision.transform.name == "HeadCollider")||
         (collision.transform.name == "HandColliderLeft(Clone)")||(collision.transform.name == "HandColliderRight(Clone)")){
 			//end = true;
+			_instance.trackingOriginTransform.position = StartPosition;
 			SceneManager.LoadScene(2);
 		}
 		
